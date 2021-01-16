8eb5cdd8db4419a07d9a8631ea901d9bc566f268
lucien1011 <lucien1011@gmail.com>
2021-01-16 04:56:50 -0500

    Added confg

---
+++ config/hzz_hel_210115_v1.py
@@ -0,0 +1,21 @@
+from utils.ObjDict import ObjDict
+
+param_dict = ObjDict(
+        template_dir = "mg_hzz_hel_template",
+        out_mg_dir = "/cmsuf/data/store/user/t2/users/klo/MLHEP/Madgraph/210115_mg_hzz_hel_v2/",
+        n_dir = 100,
+        n_evt = 100000,
+        param_low = 0.,
+        param_high = 0.2,
+        param_name = 'CWWCOUPLING',
+
+        mg_run_name = "run_01",
+        mg_slurm_script_name = "slurm.sh",
+        mg_job_name = "210115_mg_hzz_hel_delphes",
+        
+        out_delphes_dir = "/cmsuf/data/store/user/t2/users/klo/MLHEP/Delphes/210115_mg_hzz_hel_v2/",
+        cmssw_dir = "/blue/avery/kinho.lo/Delphes/CMSSW_10_0_5/src/",
+        delphes_dir = "/blue/avery/kinho.lo/Delphes/delphes/",
+        delphes_job_name = "210115_mg_hzz_hel_delphes"
+
+        )
